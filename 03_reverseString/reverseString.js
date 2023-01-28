3f485499d6faaba9709c94702cd69562227dba7a
eclavel <113940257+eclavel@users.noreply.github.com>
2023-01-28 13:12:52 -0800

    First 7 exercises passed all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var reversed = '';
+    let len = str.length - 1;
+    for(let i = len; i >= 0; i--){
+        reversed += str[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    len = arr.length;
+    for(let i = 0; i < arguments.length; i++){
+        for(let j = 0; j < len; j++){
+            if(arguments[i] === arr[j]){
+                arr.splice(j,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
